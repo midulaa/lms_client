417198647e7ba7b5afc1b67dea4f6876cc2dff66
midulaa <midulaa.s2024csbs@sece.ac.in>
2025-12-04 15:54:10 +0530

    fourth day 2.0

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-let a=0;
-    function cli(){
-        const headings=document.getElementById("data")
-        headings.innerText=a++;
-        headings.style.display="inline";
-        headings.style.backgroundColor="grey";
-        headings.style.color="pink";
-
-    }
-    
+const col=function(){
+    const div=document.getElementById("cont")
+    div.style.backgroundColor="blue"
+}
